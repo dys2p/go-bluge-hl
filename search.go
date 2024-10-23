6cb518cb45079bfc3675ea21f2dcc3d94290b75a
d2ro <d2ro@proxysto.re>
2024-10-23 19:57:11 +0200

    refactor: request.go

--- search.go
+++ search.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"html/template"
 	"strconv"
-	"strings"
 
 	"github.com/blugelabs/bluge"
 	"github.com/blugelabs/bluge/search/highlight"
@@ -103,36 +102,4 @@ func (pool *Pool[T]) Search(request *bluge.TopNSearch) ([]Result[T], error) {
 		})
 	}
 	return results, nil
-}
-
-func Fuzzy(input string, max int) *bluge.TopNSearch {
-	input = string(Normalize([]byte(input)))
-	words := strings.Fields(input)
-	if len(words) > 5 {
-		words = words[:5]
-	}
-
-	query := bluge.NewBooleanQuery()
-	for _, word := range words {
-		wordQuery := bluge.NewBooleanQuery()
-		wordQuery.AddShould(bluge.NewFuzzyQuery(word).SetField("_all").SetFuzziness(1))
-		wordQuery.AddShould(bluge.NewPrefixQuery(word).SetField("_all"))
-		wordQuery.AddShould(bluge.NewWildcardQuery("*" + word + "*").SetField("_all"))
-		query.AddMust(wordQuery)
-	}
-	return bluge.NewTopNSearch(max, query)
-}
-
-func Prefix(input string, max int) *bluge.TopNSearch {
-	input = string(Normalize([]byte(input)))
-	words := strings.Fields(input)
-	if len(words) > 5 {
-		words = words[:5]
-	}
-
-	query := bluge.NewBooleanQuery()
-	for _, word := range words {
-		query.AddMust(bluge.NewPrefixQuery(word).SetField("_all"))
-	}
-	return bluge.NewTopNSearch(max, query)
 }
